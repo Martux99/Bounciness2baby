c95dc3851738da23186214341e54004505f09066
Martux99 <34321961+Martux99@users.noreply.github.com>
2018-12-11 21:28:35 -0600

    Ahora si(no)

---
+++ Bouncie Bear/Assets/Scripts/Cajota.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cajota : MonoBehaviour {
+
+    Rigidbody2D player;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject.GetComponent<Rigidbody2D>();
+            player.gravityScale = 0;
+            player.velocity = new Vector2(0, 0);
+            player.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + 0.5f);
+            
+        }
+    }
+
+}
---
+++ Bouncie Bear/Assets/Scripts/PullAndRelease.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullAndRelease : MonoBehaviour
+{
+
+    Vector2 startPos;
+
+    void OnMouseDrag()
+    {
+        Vector2 p = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float radius = 1f;
+        Vector2 dir = p - startPos;
+        if (dir.sqrMagnitude > radius)
+        {
+            dir = dir.normalized * radius;
+        }
+
+        transform.position = startPos + dir;
+        Debug.Log(startPos + dir);
+    }
+
+    public float force = 1300;
+
+    void OnMouseUp()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = false;
+
+        Vector2 dir = startPos - (Vector2)transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dir * force);
+        this.enabled = false;
+    }
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+
+    }
+}
